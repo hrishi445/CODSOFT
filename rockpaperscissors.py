d381fe47fcc799907d9beee34202ab66bbda2825
Hrishikesh Chaudhari <103143986+hrishi445@users.noreply.github.com>
2024-01-08 23:17:26 +0530

    Add files via upload

---
+++ rockpaperscissors.py
@@ -0,0 +1,33 @@
+import random
+
+while True:
+    user_choice = input("enter your choice(rock/papers/scissors): ")
+    random_actions = ["rock", "paper", "scissors"]
+    computer_choice = random.choice(random_actions)
+    
+    print(f"your choice was {user_choice} and the computer's choice was {computer_choice}")
+
+    if user_choice == computer_choice:
+        print("It is a draw")
+    
+    elif user_choice == "rock":
+        if computer_choice == "scissors":
+            print("you win!, computer lost")
+        else:
+            print("you lost, computer wins!")
+    
+    elif user_choice == "paper":
+        if computer_choice == "rock":
+            print("you win!, computer lost")
+        else:
+            print("you lost, computer wins!")
+    
+    elif user_choice == "scissors":
+        if computer_choice == "paper":
+            print("you win!, computer lost")
+        else:
+            print("you lost, computer wins!")
+
+    restart = input("Restart(y/n):")
+    if restart.lower() != "y":
+        break
